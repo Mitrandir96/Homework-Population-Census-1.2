5062f88905e21b729a9fc7fbbb24a0d20715a55d
Илья Грачев <grachev.ia96@gmail.com>
2022-06-06 23:57:02 +0700

    added src

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.stream;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+        Stream<Person> underage = persons.stream();
+        Stream<Person> conscripts = persons.stream();
+        Stream<Person> ableToWork = persons.stream();
+        underage
+                .filter(x -> x.getAge() < 18)
+                .count();
+
+        conscripts
+                .filter(x -> x.getAge() <= 27)
+                .filter(x -> x.getAge() >= 18)
+                .map(x -> x.getFamily())
+                .collect(Collectors.toList());
+
+        List<Person> filteredPerson = ableToWork
+                .filter(x -> x.getEducation().equals(Education.HIGHER))
+                .filter(x -> (x.getSex().equals(Sex.MAN) && x.getAge() >= 18 && x.getAge() < 65) ||
+                        (x.getSex().equals(Sex.WOMAN) && x.getAge() >= 18 && x.getAge() < 60))
+                .sorted(Comparator.comparing(x -> x.getFamily()))
+                .collect(Collectors.toList());
+
+        System.out.println(filteredPerson);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
